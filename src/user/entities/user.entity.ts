9e9ce5cdfe53b6b2e92f388730f8192445b34339
sinhong jeong <jsh719803@gmail.com>
2024-05-02 09:47:42 +0900

    🩹 Git pull 후 에러 발생한 곳 수정

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,7 +5,6 @@ import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 import { TourLike } from 'src/tour/entities/like.entity';
 import { Tour } from 'src/tour/entities/tour.entity';
 import { TourType } from 'src/tour/types/tourtypes.enum';
-import { OneToMany, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, JoinColumn } from 'typeorm';
 import { Review } from 'src/reviews/entities/review.entity';
 import { Chat } from 'src/chat/entities/chat.entity';
 import { ChatTalk } from 'src/chat/entities/chattalk.entity';
@@ -61,9 +60,9 @@ export class User {
   // @Column({ type: 'enum', enum: TourType })
   // tourType: TourType;
 
-  @OneToMany(() => Review, reviews => reviews.user)
+  @OneToMany(() => Review, (reviews) => reviews.user)
   @JoinColumn({ name: 'reviewId', referencedColumnName: 'id' })
-  review : Review;
+  review: Review;
 
   @IsString()
   @IsNotEmpty({ message: '투어타입을 입력해주세요' })
